f9f854803feb278726f38ab985f7cd97b5c88d74
Deali-KimHG <asdf0185@deali.net>
2021-06-25 11:51:45 +0900

    UI: Update page with thymeleaf

---
+++ src/main/java/net/deali/intern/presentation/controller/PageController.java
@@ -0,0 +1,12 @@
+package net.deali.intern.presentation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping("/")
+    public String home() {
+        return "ajax";
+    }
+}
