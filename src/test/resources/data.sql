8942cf16a2bc76ea1dde329efa2e26ec551a86b4
Deali-KimHG <asdf0185@deali.net>
2021-06-25 18:59:48 +0900

    Test: Update Test DB schema, data

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,7 +1,7 @@
 insert into creative (title, price, exposure_start_date, exposure_end_date, status, created_date, updated_date)
-values ('테스트데이터1', 1, '2021-06-01T17:00', '2021-06-11T17:00', 0, CURRENT_DATE, CURRENT_DATE);
-insert into creative (title, price, exposure_start_date, exposure_end_date, status)
-values ('테스트데이터2', 2, '2021-06-02T17:00', '2021-06-12T17:00', 0, CURRENT_DATE, CURRENT_DATE);
+values ('테스트데이터1', 1, '2021-06-01T17:00', '2021-06-11T17:00', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+insert into creative (title, price, exposure_start_date, exposure_end_date, status, created_date, updated_date)
+values ('테스트데이터2', 2, '2021-06-02T17:00', '2021-06-12T17:00', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 insert into creative_image (name, extension, size, creative_id)
 values ('first.txt', 'txt', 10, 1);
