8abb1e6bab051879e5d3fbd62467702713414e1a
Deali-KimHG <asdf0185@deali.net>
2021-07-20 12:43:55 +0900

    UI: Update advertise ui for content number

--- src/main/resources/static/js/advertise.js
+++ src/main/resources/static/js/advertise.js
@@ -5,15 +5,18 @@ window.onload = function () {
         contentType: "application/json"
     }).done(function(data) {
         let str = '';
+        let idx = 1;
         $.each(data, function(i) {
             str += '<tr>';
+            str += '<td>' + idx + '</td>';
             str += '<td>' + data[i].title + '</td>';
             str += '<td><img src="http://localhost:8081/images/' + data[i].image + '" style="width: 200px; height: 200px;"></td>';
             str += '<td>' + data[i].price + '</td>';
-            str += '<td>' + data[i].createdDate + '</td>';
+            str += '<td>' + data[i].advertiseEndDate + '</td>';
             str += '<td>' + data[i].updatedDate + '</td>';
             str += '<td>' + data[i].score + '</td>';
             str += '</tr>';
+            idx++;
         });
         $('#creativeList').append(str);
     }).fail(function(err) {
